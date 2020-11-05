6a3c8d565af65bf7fee412c01b0895c00c0743ce
Florian Kluth <florian.kluth@hochschule-stralsund.de>
2020-11-05 13:48:41 +0100

    #193 add functionality so the harpoon rotates to the mouse

---
+++ Assets/Scripts/Harpoon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harpoon : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        var difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        difference.Normalize();
+
+        var rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
