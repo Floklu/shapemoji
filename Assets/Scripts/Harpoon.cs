3249c363d3a1649fda89d2a5e39eebdd7cb0875f
Florian Kluth <florian.kluth@hochschule-stralsund.de>
2020-11-05 19:22:28 +0100

    #193 add mouse dragging functionality and remove constraints because of physics bug

--- Assets/Scripts/Harpoon.cs
+++ Assets/Scripts/Harpoon.cs
@@ -2,37 +2,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class Harpoon : MonoBehaviour
 {
-    private float startRotationZ;
+    [SerializeField] private float startRotationZ;
+    [SerializeField] private float harpoonRange = 45f;
 
-    private void FixedUpdate()
-    {
-        var difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-
-        difference.Normalize();
-
-        var rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-
-        if (rotationZ > startRotationZ + 45f)
-        {
-            rotationZ = startRotationZ + 45f;
-        }
-        else if (rotationZ < startRotationZ - 45f)
-        {
-            rotationZ = startRotationZ - 45f;
-        }
-
-        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
-    }
-
+    private bool onHarpoon;
 
     // Start is called before the first frame update
     void Start()
     {
-        startRotationZ = transform.rotation.z;
+        var eulerAngle = transform.eulerAngles.z;
+        startRotationZ = transform.eulerAngles.z;
     }
+
+
+    private void OnMouseDown()
+    {
+        onHarpoon = true;
+    }
+
+    private void OnMouseUp()
+    {
+        onHarpoon = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (onHarpoon)
+        {
+            var difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+            difference.Normalize();
+
+            // // TODO: dont hardcode 45f 
+            // var rotationZ = nfmod(((Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg) - 45f), 360);
+            var rotationZ = (Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg) - 45f;
+
+            // if (rotationZ > nfmod(startRotationZ + harpoonRange, 360))
+            // {
+            //     rotationZ = nfmod(startRotationZ + harpoonRange, 360);
+            // }
+            // else if (rotationZ < nfmod(startRotationZ - harpoonRange, 360))
+            // {
+            //     rotationZ = nfmod(startRotationZ - harpoonRange, 360);
+            // }
+
+            Debug.Log(rotationZ);
+
+            transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        }
+    }
+
+    float nfmod(float a, float b)
+    {
+        return (float) (a - b * Math.Floor(a / b));
+    }
+
 
     // Update is called once per frame
     void Update()
