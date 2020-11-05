5574f84d69e96ab51e0547db6eaacc1f933aacc8
Robert Meier <robert.meier@hochschule-stralsund.de>
2020-11-05 22:36:24 +0100

    #249: added StoneSpawner and Factory class for creation of stones #248: maximum of stones on the playground managed by StoneSpawner

---
+++ Assets/Scripts/StoneFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+/**
+ * Factory Class, which spawns Stones onto the playground
+ */
+public class StoneFactory: MonoBehaviour
+{
+
+    public List<GameObject> preFabs;
+
+    private void Start()
+    {
+    }
+
+    /**
+     * creates random stone from selection of prefabs and instantiates stone on the playground
+     * 
+     * @param x X-Position on the playground
+     * @param y Y-Position on the playground
+     * @returns GameObject of a Stone
+     */
+    public GameObject CreateStone(float x, float y)
+    {
+        Random randomPrefab = new Random();
+        GameObject newStone = preFabs[randomPrefab.Next(preFabs.Count)];
+        Vector3 spawnPosition = new Vector3(x, y, 0);
+
+        Instantiate(newStone, spawnPosition, Quaternion.identity);
+        
+        return newStone;
+    }
+}
---
+++ Assets/Scripts/StoneSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Class, which manages the avaiable stones on the playground
+ */
+public class StoneSpawner : MonoBehaviour
+{
+
+    private int MAX_STONES = 20;
+    
+    public StoneFactory factory;
+    public GameObject spawnArea;
+
+    private List<GameObject> _stones;
+    private System.Random _randomizer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _stones = new List<GameObject>();
+        _randomizer = new System.Random();
+        InvokeRepeating(nameof(CreateRandomStone), 1f, 1f);
+        for (int i = 0; i < 10; i++)
+        {
+            CreateRandomStone();
+        }
+    }
+
+    /*
+     * creates a stone at a random location
+     */
+    void CreateRandomStone()
+    {
+        if (_stones.Count < MAX_STONES)
+        {
+            Vector3 spawnPosition = spawnArea.transform.position;
+            Vector3 spawnBounds = spawnArea.transform.localScale;
+            
+            float x = spawnPosition.x - (spawnBounds.x / 2);
+            float y = spawnPosition.y - (spawnBounds.y / 2);
+            
+            x = x + (float)(_randomizer.NextDouble()) * spawnBounds.x;
+            y = y + (float)(_randomizer.NextDouble()) * spawnBounds.y;
+            
+            GameObject stone = factory.CreateStone(x, y);
+            _stones.Add(stone);
+        }
+    }
+}
