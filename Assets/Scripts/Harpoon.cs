30f0fafbcc15e7b1ad9bf0b23a38ce05acad244e
Florian Kluth <florian.kluth@hochschule-stralsund.de>
2020-11-05 14:46:52 +0100

    #193 add constraints to harpoon rotation

--- Assets/Scripts/Harpoon.cs
+++ Assets/Scripts/Harpoon.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Harpoon : MonoBehaviour
 {
+    private float startRotationZ;
+
     private void FixedUpdate()
     {
         var difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
@@ -13,12 +15,23 @@ public class Harpoon : MonoBehaviour
 
         var rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
+        if (rotationZ > startRotationZ + 45f)
+        {
+            rotationZ = startRotationZ + 45f;
+        }
+        else if (rotationZ < startRotationZ - 45f)
+        {
+            rotationZ = startRotationZ - 45f;
+        }
+
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
     }
+
 
     // Start is called before the first frame update
     void Start()
     {
+        startRotationZ = transform.rotation.z;
     }
 
     // Update is called once per frame
