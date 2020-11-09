b8b260475fbe473a7b36b90f2b8424cde2e379fd
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-11-09 09:00:11 +0100

    Implemented Workshop

---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    private Collider2D _currentWorkShop;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("WorkArea"))
+        {
+            _currentWorkShop = other;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("WorkArea") && other==_currentWorkShop)
+        {
+            _currentWorkShop = null;
+        }
+    }
+
+
+    /// <summary>
+    /// Drag Method
+    /// </summary>
+    private void OnMouseDrag()
+    {
+        if (Camera.main != null)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (_currentWorkShop != null)
+        {
+            _currentWorkShop.gameObject.GetComponent<WorkArea>().PutObject();
+        }
+    }
+}
---
+++ Assets/Scripts/WorkArea.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WorkArea : MonoBehaviour
+{
+    private Collider2D _workObject;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Stone"))
+        {
+            _workObject = other;
+        }
+    }
+
+    public void PutObject()
+    {
+        if (_workObject != null)
+        {
+            _workObject.transform.Translate(transform.position - _workObject.transform.position);
+        }
+    }
+}
