d594bb9b95925bbf00dc0814865347f7c0ad3e26
Robert Meier <robert.meier@hochschule-stralsund.de>
2020-11-10 21:08:36 +0100

    #321 removed issues raised by Rider added Property for screenHeight, and screenWidth to configure aspect ratio

--- src/Assets/Scripts/CameraSetting.cs
+++ src/Assets/Scripts/CameraSetting.cs
@@ -1,87 +1,106 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
- 
-/// <summary>
-/// The script is responsible for the resolution of the camera
-/// based on source: https://forum.unity.com/threads/force-camera-aspect-ratio-16-9-in-viewport.385541/
-/// </summary>
+
+/**
+ * The script is enforces the aspect ratio of the given height and width of the screen
+   based on source: https://forum.unity.com/threads/force-camera-aspect-ratio-16-9-in-viewport.385541/
+ */
 public class CameraSetting : MonoBehaviour
 {
- 
- 
-    #region Variables
-    private int ScreenSizeX = 0;
-    private int ScreenSizeY = 0;
+    #region Properties
+
+    public int screenHeight;
+    public int screenWidth;
+
     #endregion
- 
+
+    #region Variables
+
+    private int _screenSizeX;
+    private int _screenSizeY;
+    private Camera _gameCamera;
+
+    #endregion
+
     #region Methods
- 
-    #region rescale camera
+
+    /**
+     * Rescales Camera based on Screen-Size and adds pillarbox to top or side to enforce aspect ratio of game
+     */
     private void RescaleCamera()
     {
- 
-        if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
- 
-        float targetaspect = 256f/135f; //4096x2160
-        float windowaspect = (float)Screen.width / (float)Screen.height;
-        float scaleheight = windowaspect / targetaspect;
-        Camera camera = GetComponent<Camera>();
- 
+        if (Screen.width == _screenSizeX && Screen.height == _screenSizeY) return;
+
+        var targetaspect = screenHeight / (float) screenWidth; //4096x2160
+        var windowaspect = Screen.width / (float) Screen.height;
+        var scaleheight = windowaspect / targetaspect;
+
         if (scaleheight < 1.0f)
         {
-            Rect rect = camera.rect;
- 
+            var rect = _gameCamera.rect;
+
             rect.width = 1.0f;
             rect.height = scaleheight;
             rect.x = 0;
             rect.y = (1.0f - scaleheight) / 2.0f;
- 
-            camera.rect = rect;
+
+            _gameCamera.rect = rect;
         }
         else // add pillarbox
         {
-            float scalewidth = 1.0f / scaleheight;
- 
-            Rect rect = camera.rect;
- 
+            var scalewidth = 1.0f / scaleheight;
+
+            var rect = _gameCamera.rect;
+
             rect.width = scalewidth;
             rect.height = 1.0f;
             rect.x = (1.0f - scalewidth) / 2.0f;
             rect.y = 0;
- 
-            camera.rect = rect;
+
+            _gameCamera.rect = rect;
         }
- 
-        ScreenSizeX = Screen.width;
-        ScreenSizeY = Screen.height;
+
+        _screenSizeX = Screen.width;
+        _screenSizeY = Screen.height;
     }
+
     #endregion
- 
-    #endregion
- 
+
     #region Unity Methods
- 
-    void OnPreCull()
+
+    /**
+     * called before camera culls a scene
+     */
+    private void OnPreCull()
     {
-        if (Application.isEditor) return;
-        Rect wp = Camera.main.rect;
-        Rect nr = new Rect(0, 0, 1, 1);
- 
-        Camera.main.rect = nr;
+        if (Application.isEditor || Camera.main is null) return;
+
+        var mainCamera = Camera.main;
+        var wp = mainCamera.rect;
+        var nr = new Rect(0, 0, 1, 1);
+
+        mainCamera.rect = nr;
         GL.Clear(true, true, Color.black);
-       
+
         Camera.main.rect = wp;
- 
     }
- 
-    // Use this for initialization
-    void Start () {
+
+    /**
+     * Initializes camera on start
+     */
+    private void Start()
+    {
+        _gameCamera = GetComponent<Camera>();
         RescaleCamera();
     }
-   
-    // Update is called once per frame
-    void Update () {
+
+    /**
+     * Update camera every frame
+     * TODO: check if rescale every frame causes performance problems
+     */
+    private void Update()
+    {
         RescaleCamera();
     }
+
     #endregion
 }
