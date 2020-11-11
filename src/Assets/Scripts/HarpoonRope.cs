253d7a35fcc4a4faa5be55c9018cbbbc7713e7f7
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-11-11 12:40:49 +0100

    fixed rope lengthening

--- src/Assets/Scripts/HarpoonRope.cs
+++ src/Assets/Scripts/HarpoonRope.cs
@@ -9,26 +9,34 @@ public class HarpoonRope : MonoBehaviour
     public GameObject cannon;
     private SpriteRenderer _spriteRenderer;
 
+    private Transform _ropeTransform;
+    private Transform _cannonTransform;
+    private Transform _projectileTransform;
+
     // Start is called before the first frame update
     void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _ropeTransform = transform;
+        _cannonTransform = cannon.transform;
+        _projectileTransform = projectile.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        Vector3 centerPos = (cannon.transform.position + projectile.transform.position) / 2f;
-        transform.position = centerPos;
-        Vector3 ropeScale = transform.localScale;
-        //scale.x = Vector3.Distance(cannon.transform.position,projectile.transform.position)/ GetComponent<SpriteRenderer>().bounds.size.x;
-        ropeScale.x *= projectile.transform.position.x / _spriteRenderer.bounds.extents.x;
-
-        ropeScale.x = Math.Abs(ropeScale.x);
         
+        Vector3 ropeCenter = (cannon.transform.position + projectile.transform.position) / 2f;
+        
+        _ropeTransform.position = ropeCenter;
+        Vector3 ropeScale = _ropeTransform.localScale;
 
-    
-        //transform.localScale = scale;
+        float requiredRopeLength = Vector3.Distance(_cannonTransform.position,_projectileTransform.position);
+        
+        float currentRopeLength = _spriteRenderer.bounds.size.x;
+     
+
+        ropeScale.x *= requiredRopeLength / currentRopeLength;
+        transform.localScale = ropeScale;
     }
 }
