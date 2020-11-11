c61130e5311fe5699b12b7a83815bef6b3024afc
Florian Kluth <florian.kluth@hochschule-stralsund.de>
2020-11-11 18:13:41 +0100

    #333 add the functionality to quit when pressing the escape key

---
+++ src/Assets/Scripts/Game.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/**
+ * Class Game has functionality to quit the game.
+ */
+public class Game : MonoBehaviour
+{
+    /**
+     * Update is called once per frame.
+     * The game is quitting if the escape key is pressed.
+     */
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // this quits the game in the unity editor
+            EditorApplication.isPlaying = false;
+            // this quits the game if it's already build and running
+            Application.Quit();
+        }
+    }
+}
