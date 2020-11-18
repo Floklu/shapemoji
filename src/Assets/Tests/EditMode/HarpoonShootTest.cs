d94ec8f3e9a9becf3f0e7a917cacbcbaee9e60bc
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-11-18 08:38:39 +0100

    created test

---
+++ src/Assets/Tests/EditMode/HarpoonShootTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Tests.EditMode
+{
+    public class HarpoonShootTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            EditorSceneManager.OpenScene($"Assets/Scenes/Scene_Playground_2vs2.unity");
+        }
+
+        /**
+         * Tests, if Harpoon Objects for Team1/Player_1 exist
+         */
+        [Test]
+        public void HarpoonShootTestObjectsExistencePasses()
+        {
+            var harpoon = GameObject.Find("Team_1/Player_1/Base/HarpoonBase/Harpoon");
+            var cannon = GameObject.Find("Team_1/Player_1/Base/HarpoonBase/Harpoon/HarpoonCannon");
+            var projectile =
+                GameObject.Find("Team_1/Player_1/Base/HarpoonBase/Harpoon/HarpoonCannon/HarpoonProjectile");
+            var rope = GameObject.Find("Team_1/Player_1/Base/HarpoonBase/Harpoon/HarpoonCannon/HarpoonRope");
+
+            Assert.NotNull(harpoon);
+            Assert.NotNull(cannon);
+            Assert.NotNull(projectile);
+            Assert.NotNull(rope);
+        }
+    }
+}
