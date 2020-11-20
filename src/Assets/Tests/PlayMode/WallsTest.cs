18db46cbb2d651f5ef31210f145a2d346ba0255f
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-11-20 13:17:01 +0100

    added tests for each corner

--- src/Assets/Tests/PlayMode/WallsTest.cs
+++ src/Assets/Tests/PlayMode/WallsTest.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -17,17 +16,84 @@ namespace Tests.PlayMode
         {
             SceneManager.LoadScene("Scenes/Scene_Playground_2vs2");
         }
+
         
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator WallsTestWithEnumeratorPasses()
+        public IEnumerator WallsTestBlueBase()
         {
-            Vector3 screenSize = new Vector3(100,100,0);
-            
+            var harpoon =
+                GameObject.Find("Team_2/Player_3/Base/HarpoonBase/Harpoon");
+            var projectile =
+                GameObject.Find("Team_2/Player_3/Base/HarpoonBase/Harpoon/HarpoonCannon/HarpoonProjectile");
+
+            aimAtWall(harpoon, projectile, Screen.width, 0,Vector3.up+Vector3.left);
+
+            projectile.GetComponent<Projectile>().Shoot();
+
+            yield return new WaitForSeconds(1.5f);
+
+            Assert.Zero(projectile.GetComponent<Rigidbody2D>().velocity.magnitude);
+        }
+        
+        
+        [UnityTest]
+        public IEnumerator WallsTestRedBase()
+        {
+            var harpoon =
+                GameObject.Find("Team_1/Player_1/Base/HarpoonBase/Harpoon");
+            var projectile =
+                GameObject.Find("Team_1/Player_1/Base/HarpoonBase/Harpoon/HarpoonCannon/HarpoonProjectile");
+
+            aimAtWall(harpoon, projectile, 0, 0,Vector3.up+Vector3.right);
+
+            projectile.GetComponent<Projectile>().Shoot();
+
+            yield return new WaitForSeconds(1.5f);
+
+            Assert.Zero(projectile.GetComponent<Rigidbody2D>().velocity.magnitude);
+        }
+        
+        [UnityTest]
+        public IEnumerator WallsTestGreenBase()
+        {
+            var harpoon =
+                GameObject.Find("Team_1/Player_2/Base/HarpoonBase/Harpoon");
+            var projectile =
+                GameObject.Find("Team_1/Player_2/Base/HarpoonBase/Harpoon/HarpoonCannon/HarpoonProjectile");
+
+            aimAtWall(harpoon, projectile, 0, Screen.height,Vector3.down+Vector3.right);
+
+            projectile.GetComponent<Projectile>().Shoot();
+
+            yield return new WaitForSeconds(1.5f);
+
+            Assert.Zero(projectile.GetComponent<Rigidbody2D>().velocity.magnitude);
+        }
+        
+        [UnityTest]
+        public IEnumerator WallsTestOrangeBase()
+        {
+            var harpoon =
+                GameObject.Find("Team_2/Player_4/Base/HarpoonBase/Harpoon");
+            var projectile =
+                GameObject.Find("Team_2/Player_4/Base/HarpoonBase/Harpoon/HarpoonCannon/HarpoonProjectile");
+
+            aimAtWall(harpoon, projectile, Screen.width, Screen.height,Vector3.down+Vector3.left);
+
+            projectile.GetComponent<Projectile>().Shoot();
+
+            yield return new WaitForSeconds(1.5f);
+
+            Assert.Zero(projectile.GetComponent<Rigidbody2D>().velocity.magnitude);
+        }
+
+        private void aimAtWall(GameObject harpoon, GameObject projectile, int pointX, int pointY,Vector3 harpoonRotation)
+        {
+            Vector3 screenSize = new Vector3(100, 100, 0);
+
             if (!(Camera.main is null))
             {
-                screenSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
+                screenSize = Camera.main.ScreenToWorldPoint(new Vector3(pointX, pointY, 0));
                 screenSize.z = 0;
             }
             else
@@ -35,31 +101,15 @@ namespace Tests.PlayMode
                 Assert.IsTrue(false);
             }
 
-
-            var harpoon =
-                GameObject.Find("Team_2/Player_3/Base/HarpoonBase/Harpoon");
-            var projectile =
-                GameObject.Find("Team_2/Player_3/Base/HarpoonBase/Harpoon/HarpoonCannon/HarpoonProjectile");
-
             Vector3 harpoonPosition = harpoon.transform.position;
 
             Vector3 path = screenSize - harpoonPosition;
+            
+            
 
-            float angle = Vector3.Angle(Vector3.up+Vector3.left, path);
-            
-            harpoon.transform.Rotate(0,0,angle);
-            
-            projectile.GetComponent<Projectile>().Shoot();
-            
-            
-            
-            yield return new WaitForSeconds(3);
+            float angle = Vector3.Angle(harpoonRotation, path);
 
-            Assert.Zero(projectile.GetComponent<Rigidbody2D>().velocity.magnitude);
-            Debug.Log(projectile.transform.position.ToString());
-            Debug.Log(screenSize.ToString());
-            
-            
+            harpoon.transform.Rotate(0, 0, angle);
         }
     }
 }
---
+++ src/Assets/Tests/PlayMode/WallsTest2.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode
+{
+    public class WallsTest2
+    {
+        /**
+         * Loads game scene
+         */
+        [OneTimeSetUp]
+        public void LoadScene()
+        {
+            SceneManager.LoadScene("Scenes/Scene_Playground_2vs2");
+        }
+        
+        
+        [UnityTest]
+        public IEnumerator WallsTestWithEnumeratorPasses()
+        {
+            Vector3 screenSize = new Vector3(100,100,0);
+            
+            if (!(Camera.main is null))
+            {
+                screenSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
+                screenSize.z = 0;
+            }
+            else
+            {
+                Assert.IsTrue(false);
+            }
+
+
+            var harpoon =
+                GameObject.Find("Team_2/Player_3/Base/HarpoonBase/Harpoon");
+            var projectile =
+                GameObject.Find("Team_2/Player_3/Base/HarpoonBase/Harpoon/HarpoonCannon/HarpoonProjectile");
+
+            Vector3 harpoonPosition = harpoon.transform.position;
+
+            Vector3 path = screenSize - harpoonPosition;
+
+            float angle = Vector3.Angle(Vector3.up+Vector3.left, path);
+            
+            harpoon.transform.Rotate(0,0,angle+12.0f);
+            
+            projectile.GetComponent<Projectile>().Shoot();
+            
+            
+            
+            yield return new WaitForSeconds(3);
+
+            Assert.Zero(projectile.GetComponent<Rigidbody2D>().velocity.magnitude);
+            
+            
+            
+            
+        }
+        
+    }
+}
