aa21d6e4ca394f207541850b6e6d4322f3dee95d
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-11-25 13:38:46 +0100

    added hook and wound in functionality

--- src/Assets/Scripts/Harpoon/HarpoonController.cs
+++ src/Assets/Scripts/Harpoon/HarpoonController.cs
@@ -12,6 +12,7 @@ namespace Harpoon
         
         private RotatableHandler _rotatableHandler;
         private HarpoonShotHandler _shotHandler;
+        private HarpoonWoundInHandler _woundInHandler;
         private ProjectileCollision _projectileCollision;
         private MovingProjectile _movingProjectile;
         private HarpoonRope _rope;
@@ -20,6 +21,8 @@ namespace Harpoon
         
         private bool _windIn;
         private Collider2D _cannonCollider; //needed to better handle collision while wound in
+        private HookableObject _objectHooked;
+        private GameObject _inventory;
 
 
         private void Start()
@@ -29,8 +32,11 @@ namespace Harpoon
             var ropeObj = gameObject.transform.Find("HarpoonCannon/HarpoonRope").gameObject;
             _crankController = gameObject.transform.Find("../../Wheel").gameObject.GetComponent<CrankController>();
 
+            _inventory = gameObject.transform.Find("../../Inventory").gameObject;
+            
             _rotatableHandler = GetComponent<RotatableHandler>();
             _shotHandler = GetComponent<HarpoonShotHandler>();
+            _woundInHandler = _cannonCollider.GetComponent<HarpoonWoundInHandler>();
             _projectileCollision = projectileObj.GetComponent<ProjectileCollision>();
             _movingProjectile = projectileObj.GetComponent<MovingProjectile>();
             _rope = ropeObj.GetComponent<HarpoonRope>();
@@ -41,7 +47,8 @@ namespace Harpoon
             _rotatableHandler.RotationEvent += OnRotationEvent;
             _shotHandler.ShotEvent += OnShotEvent;
             _projectileCollision.CollisionEvent += ProjectileOnCollisionEvent;
-            
+            _woundInHandler.CollisionEvent += OnWoundInEvent;
+
         }
 
         /**
@@ -63,6 +70,7 @@ namespace Harpoon
             {
                 ResetCannon();
                 //TODO handle WoundIn specific behaviour
+                
             }
             else //Harpoon hasn't been wound in
             {
@@ -115,11 +123,39 @@ namespace Harpoon
         */
         public void NotifyCollisionWithHookableObject(HookableObject hookableObject, GameObject projectile)
         {
-            throw new NotImplementedException();
+            var projectileObj = gameObject.transform.Find("HarpoonCannon/HarpoonProjectile").gameObject;
+
+            if (projectile == projectileObj)
+            {
+                if (hookableObject.GetType() == typeof(Stone))
+                {
+                    _objectHooked = hookableObject;
+                    _movingProjectile.AttachObject(hookableObject.gameObject);
+                
+                } 
+                else if (hookableObject.GetType() == typeof(Item))
+                {
+                    _objectHooked = hookableObject;
+                    _movingProjectile.AttachObject(hookableObject.gameObject);
+                }
+            }
+
         }                
          
         
         #region EventHandling
+
+        private void OnWoundInEvent(object sender, EventArgs eventArg)
+        {
+            if (_objectHooked != null)
+            {
+                _movingProjectile.UnattachObject();
+                HookableObjectController.OnWoundIn(_objectHooked,_inventory);
+            }
+
+            _objectHooked = null;
+            Debug.Log("Wound In");
+        }
         
         /**
          * implements CollisionEvent
---
+++ src/Assets/Scripts/Harpoon/HarpoonWoundInHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Harpoon
+{
+    public class HarpoonWoundInHandler : MonoBehaviour
+    {
+        private Collider2D _collider;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.transform.parent.gameObject == this.gameObject)
+            {
+                OnWoundInEvent();
+                Debug.Log("WoundIn Trigger");
+            }
+            
+        }
+        
+        #region Events
+
+        /**
+         * invokes CollisionEvent, if harpoon collides with cannon collider
+         */
+        public event EventHandler CollisionEvent;
+        
+        /**
+         * raises Event CollisionEvent
+         */
+        protected virtual void OnWoundInEvent()
+        {
+            CollisionEvent?.Invoke(this, EventArgs.Empty);
+        }
+        
+        #endregion
+        
+    }
+}
+
