7b9f804292ff219496c2340624bcf530083d11c7
Florian Kluth <florian.kluth@hochschule-stralsund.de>
2020-11-25 17:59:22 +0100

    #451 add workshop prefab and script

---
+++ src/Assets/Scripts/Workshop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Class Workshop contains the workshop functionality
+ */
+public class Workshop : MonoBehaviour
+{
+    [SerializeField] private GameObject myBase;
+    
+    
+}
