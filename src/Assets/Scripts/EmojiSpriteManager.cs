35d3400872e062f4af8e87dc04a5b2cf4f9e59c4
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-12-06 20:37:17 +0100

    created sprite manager class

---
+++ src/Assets/Scripts/EmojiSpriteManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/**
+ * This class is used to get new Emoji-Sprites for the Score Area
+ */
+public class EmojiSpriteManager : MonoBehaviour
+{
+    private Game _game;
+    private int _currentEmojiNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _game = Game.Instance;
+        _currentEmojiNumber = -1;
+    }
+
+    /**
+     * Method to change current Emoji to the next one from the Sprite List
+     * @return new Emoji-Sprite
+     */
+    public Sprite GetNewEmoji()
+    {
+        _currentEmojiNumber++;
+        Sprite newEmoji = _game.GetEmoji(_currentEmojiNumber);
+        return newEmoji;
+    }
+    
+    
+}
