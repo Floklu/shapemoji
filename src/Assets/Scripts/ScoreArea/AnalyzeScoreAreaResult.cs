4a789f7719f5820373a61136736636efaafda7d1
worksasintended <marc@marschalls.net>
2020-12-07 23:01:01 +0100

    not working yet

---
+++ src/Assets/Scripts/ScoreArea/AnalyzeScoreAreaResult.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ScoreArea
+{
+    public class AnalyzeScoreAreaResults
+    {
+        private int _emojiCovered;
+        private int _emojiUncovered;
+        private int _backgroundCovered;
+
+        public void SetEmojiCovered(int i)
+        {
+            _emojiCovered = i;
+        }
+
+        public void SetEmojiUncovered(int i)
+        {
+            _emojiUncovered = i;
+        }
+
+        public void SetBackgroundCovered(int i)
+        {
+            _backgroundCovered = i;
+        }
+
+        public int GetEmojiCovered()
+        {
+            return _emojiCovered;
+        }
+
+        public int GetEmojiUncovered()
+        {
+            return _emojiUncovered;
+        }
+
+        public int GetBackgroundCovered()
+        {
+            return _backgroundCovered;
+        }
+        
+    }
+}
---
+++ src/Assets/Scripts/ScoreArea/ScoreCalculation.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+namespace ScoreArea
+{
+    public class ScoreCalculation : MonoBehaviour
+    {
+        public AnalyzeScoreAreaResults AnalyzeScoreableView(ScoreArea scoreArea)
+        {
+            var result = new AnalyzeScoreAreaResults();
+            var renderer = scoreArea.GetComponent<Renderer>();
+            var bounds = renderer.bounds;
+            // get dimensions of Score Area
+            var size = bounds.size;
+            var position = bounds.min;
+            // create texture to store "screenshot" in
+            var img = new Texture2D((int) size.x, (int) size.y, TextureFormat.RGB24, false);
+            // create rectengular at score area position
+            var rect = new Rect((Vector2)position, (Vector2)size);
+            // create the image
+            img.ReadPixels(rect, 0, 0);
+            img.Apply();
+            var pixels = img.GetPixels();
+            
+            return result;
+        }
+   
+
+        
+        
+    }
+}
