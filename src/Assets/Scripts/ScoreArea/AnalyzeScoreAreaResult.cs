878fe85d6e3d2463239649495db264c98e8d6bb4
Marc <marcburock@arcor.de>
2020-12-10 12:48:50 +0100

    use auto property

--- src/Assets/Scripts/ScoreArea/AnalyzeScoreAreaResult.cs
+++ src/Assets/Scripts/ScoreArea/AnalyzeScoreAreaResult.cs
@@ -6,26 +6,10 @@
      */
     public class AnalyzeScoreAreaResult
     {
-        private int _emojiCovered;
-        private int _emojiUncovered;
-        private int _backgroundCovered;
+        public int EmojiCovered { get; set; }
 
-        public int EmojiCovered
-        {
-            get => _emojiCovered;
-            set => _emojiCovered = value;
-        }
+        public int EmojiUncovered { get; set; }
 
-        public int EmojiUncovered
-        {
-            get => _emojiUncovered;
-            set => _emojiUncovered = value;
-        }
-
-        public int BackgroundCovered
-        {
-            get => _backgroundCovered;
-            set => _backgroundCovered = value;
-        }
+        public int BackgroundCovered { get; set; }
     }
 }
