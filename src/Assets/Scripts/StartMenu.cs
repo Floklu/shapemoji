9b3798c0fc46ee1e92bdb9500b06c7a51045d0c5
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-12-11 15:02:56 +0100

    fixed click event handling created class Start Menu

---
+++ src/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/**
+ * StartMenu Class has Methods for each Menu Option
+ */
+public class StartMenu : MonoBehaviour
+{
+    /**
+     * Run menu option: start game
+     */
+    public void StartGame()
+    {
+        GameSceneManager.Instance.LoadPlayingScene();
+    }
+    
+}
