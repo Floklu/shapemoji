79d66e513dda50d5ccd2614f1be0e74c897f273d
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-12-11 15:35:20 +0100

    added exit method

--- src/Assets/Scripts/StartMenu.cs
+++ src/Assets/Scripts/StartMenu.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 /**
  * StartMenu Class has Methods for each Menu Option
@@ -11,6 +12,20 @@ public class StartMenu : MonoBehaviour
     public void StartGame()
     {
         GameSceneManager.Instance.LoadPlayingScene();
+    }
+
+    /**
+     * Exit menu option: exit the application
+     * Exit code by Florian Kluth
+     */
+    public void Exit()
+    {
+        // this quits the game in the unity editor
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+        // this quits the game if it's already build and running
+        Application.Quit();
     }
     
 }
