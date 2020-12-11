719dc74b53b08609c3e39feb07f27167de972766
Andrei Dziubenka <Andrei.Dziubenka@hochschule-stralsund.de>
2020-12-11 09:00:16 +0100

    created Start Menu TODO: fix button OnMouseUp

---
+++ src/Assets/StartButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/**
+ * Class StartButton for the Button Start in the Start Menu
+ */
+public class StartButton : MonoBehaviour
+{
+    /**
+     * Is triggered when mouse is up
+     * Loads playing scene through GameSceneManager
+     */
+    private void OnMouseUp()
+    {
+        Debug.Log("OnMouseUp");
+        GameSceneManager.Instance.LoadPlayingScene();
+    }
+}
