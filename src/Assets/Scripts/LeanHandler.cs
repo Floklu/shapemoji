c132ffca12df1d5bc050e641f6c908f290672073
worksasintended <marc@marschalls.net>
2020-12-14 12:30:37 +0100

    add LeanHandler.cs

---
+++ src/Assets/Scripts/LeanHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lean.Touch;
+using UnityEngine;
+
+public class LeanHandler 
+{
+    /**
+     * MakeDraggable adds the Lean Touch Scripts and functionality to the gameObject to make it draggable
+     */
+    public static void MakeDraggable(GameObject gameObject)
+    {
+        //set selectable
+        gameObject.AddComponent<LeanSelectable>();
+        //move on drag
+        gameObject.AddComponent<LeanDragTranslate>();
+        //set deselectable
+        var leanSelectable = gameObject.GetComponent<LeanSelectable>();
+        leanSelectable.DeselectOnUp = true;
+        leanSelectable.IsolateSelectingFingers = true;
+    }
+}
